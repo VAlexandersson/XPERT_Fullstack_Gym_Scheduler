e15869b66c8fa0464db7530f4b0360ddc2ac0018
Valexanderman <viktor.alexand@gmail.com>
2023-10-10 12:48:58 +0200

    restruct

--- frontend/src/containers/ExercisePage.tsx
+++
@@ -1,6 +0,0 @@
-const ExercisePage: React.FC = () => {
-    return (
-        <h1>EXERCISES</h1>
-    )
-};
-export default ExercisePage;
--- frontend/src/containers/LoginPage.tsx
+++
@@ -1,6 +0,0 @@
-const LoginPage: React.FC = () => {
-    return (
-        <h1>Login</h1>
-    )
-};
-export default LoginPage;
