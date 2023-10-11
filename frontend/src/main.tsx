5a2d67b54cc5f7f1cd9b9541a7e79dbb7edafc85
MartinKarlsson1337 <markah213@studentmail.oru.se>
2023-10-11 09:44:12 +0200

    Trying to connect frontend with backend

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,13 +4,9 @@ import { ThemeProvider } from "@emotion/react";
 import { CssBaseline } from "@mui/material";
 import theme from "./theme";
 import App from "./App";
+import LoginPage from "./LoginPage";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <App />
-    </ThemeProvider>
-  </React.StrictMode>
+      <App></App>
 );
 
