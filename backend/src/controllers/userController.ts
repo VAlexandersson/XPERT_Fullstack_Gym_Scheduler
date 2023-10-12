7221a1dbeb9624b36e9117032929daab38f114ed
Viktor Alexandersson <viktor.alexand@gmail.com>
2023-10-12 20:52:51 +0200

    added restful crud operations

--- backend/src/controllers/userController.ts
+++ backend/src/controllers/userController.ts
@@ -1,45 +1,58 @@
 import { Request, Response } from 'express';
 import db from '../database/db';
 
+
 export const getAllUsers = (req: Request, res: Response) => {
     db.all('SELECT * FROM User', (err, rows) => {
         if (err)                return res.status(500).json({ message: 'Error getting users' });
         if (rows.length > 0)    return res.status(200).json({ message: rows } );
         return res.status(404).json({ message: 'No users found' });
     });
+    
 }
 
-export const createUser = (req: Request, res: Response) => {
-    const { username, gym_id, password } = req.body;
-    db.get('SELECT * FROM User WHERE ID = ?', [gym_id], (err, row) => {
-        if (err) return res.status(500).json({ message: 'Error creating user' }); 
-        if (row) return res.status(409).json({ message: 'User already exists' });
-        
-        db.run('INSERT INTO User (ID, username ,password) VALUES (?, ?, ?)', [gym_id, username, password], (err) => {
-            if (err) return res.status(500).json({ message: 'Error creating user' }); 
-            return res.status(201).json({ message: 'Success User created' });
-        });
-    });
-}
-
-
-export const loginUser = (req: Request, res: Response) => {
-    const { gym_id, password } = req.body;
-    db.get('SELECT * FROM User WHERE username = ? AND password = ?', [gym_id, password], (err, row) => {
+export const getUser = (req: Request, res: Response) => {
+    const id: number = parseInt(req.params.id);
+    db.get('SELECT * FROM User WHERE ID = ?', [id], (err, row) => {
         if (err) return res.status(500).json({ message: 'Error getting user' });
         if (row) return res.status(201).json({ message: 'Success', user: row });
         return res.status(404).json({ message: 'User not found' });
     })
-};
+}
+
+export const loginUser = (req: Request, res: Response) => {
+    const { gym_id, password } = req.body;
+    console.log(gym_id, password);
+    db.get('SELECT * FROM User WHERE ID = ? AND password = ?', [parseInt(gym_id), password], (err, row) => {
+        if (err) return res.status(500).json({ message: 'Error logging in' });
+        if (row) return res.status(200).json({ message: 'Success', user: row });
+        return res.status(404).json({ message: 'User not found' });
+    })
+}
+
+export const createUser = (req: Request, res: Response) => {
+    const id: number = parseInt(req.params.id);
+    const { username, password } = req.body;
+    db.get('SELECT * FROM User WHERE ID = ?', [id], (err, row) => {
+        if (err) return res.status(500).json({ message: 'Error creating user' }); 
+        if (row) return res.status(409).json({ message: 'User already exists' });
+        
+        db.run('INSERT INTO User (ID, username ,password) VALUES (?, ?, ?)', [id, username, password], (err) => {
+            if (err) return res.status(500).json({ message: 'Error creating user' }); 
+        });
+        return res.status(201).json({ message: 'Success User created' });
+    });
+}
 
 
 export const deleteUser = (req: Request, res: Response) => {
-    const { gym_id } = req.body;
-    console.log('deleteUser'+ gym_id);
-    db.get('SELECT * FROM User WHERE ID = ?', [gym_id], (err, row) => {
+    const id: number = parseInt(req.params.id);
+    db.get('SELECT * FROM User WHERE ID = ?', [id], (err, row) => {
         if (err) return res.status(500).json({ message: 'Error deleting user' });
         if (row) {
-            db.run('DELETE FROM User WHERE ID = ?', [gym_id], (err) => { if (err) return res.status(500).json({ message: 'Error deleting user' }); });
+            db.run('DELETE FROM User WHERE ID = ?', [id], (err) => { 
+                if (err) return res.status(500).json({ message: 'Error deleting user' }); 
+            });
             return res.status(202).json({ message: 'Success user deleted' });
         }
         return res.status(404).json({ message: 'User not found' });
@@ -48,15 +61,15 @@ export const deleteUser = (req: Request, res: Response) => {
 
 
 export const updateUser = (req: Request, res: Response) => {
-    const { gym_id, username, password } = req.body;
-    db.get('SELECT * FROM User WHERE ID = ?', [gym_id], (err, row) => {
+    const id: number = parseInt(req.params.id);
+    const { username, password } = req.body;
+    db.get('SELECT * FROM User WHERE ID = ?', [id], (err, row) => {
         if (err)  return res.status(500).json({ message: 'Error updating user' });
-        if (row) {
-            db.run('UPDATE User SET username = ?, password = ? WHERE ID = ?', [username, password, gym_id], (err) => {
-                if (err) return res.status(500).json({ message: 'Error updating user' });
-            });
-            return res.status(201).json({ message: 'Success user updated' });
-        }
-        return res.status(404).json({ message: 'User not found' });
+        if (!row) return res.status(404).json({ message: 'User not found' });
+        db.run('UPDATE User SET username = ?, password = ? WHERE ID = ?', [username, password, id], (err) => {
+            if (err) return res.status(500).json({ message: 'Error updating user' });
+        });
+        return res.status(201).json({ message: 'Success user updated' });
     });
 }
+
--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,13 +1,24 @@
 import {Router} from 'express';
 
 import * as userController from '../controllers/userController';
+import * as catalogExerciseController from '../controllers/catalogExerciseController';
+import * as userExerciseController from '../controllers/userExerciseController';
 
 const router:Router = Router();
 
 export default router;
 
-router.route('/createUser').post(userController.createUser);
 router.route('/login').post(userController.loginUser);
-router.route('/deleteUser').delete(userController.deleteUser);
-router.route('/getAllUsers').get(userController.getAllUsers);
-router.route('/updateUser').put(userController.updateUser);
+
+router.route('/users').get(userController.getAllUsers);
+router.route('/users/:id').get(userController.getUser);
+router.route('/users').post(userController.createUser);
+router.route('/users/:id').delete(userController.deleteUser);
+router.route('/users/:id').put(userController.updateUser);
+
+
+router.route('/catalog_exercise').get(catalogExerciseController.getAllExercises);
+router.route('/catalog_exercise/:id').get(catalogExerciseController.getExercise);
+router.route('/catalog_exercise').post(catalogExerciseController.createExercise);
+router.route('/catalog_exercise/:id').delete(catalogExerciseController.deleteExercise);
+router.route('/catalog_exercise/:id').put(catalogExerciseController.updateExercise);
